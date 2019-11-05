5829999c961dbd9a29217e79f1da641b19c4ad43
Connor Kline <32586807+ckline1@users.noreply.github.com>
2019-11-05 10:53:34 -0500

    Added text for announcer

--- Assets/UIController.cs
+++ Assets/UIController.cs
@@ -6,8 +6,18 @@ using UnityEngine.UI;
 public class UIController : MonoBehaviour
 {
     [SerializeField] Text ScoreText;
+    [SerializeField] Text announcerText;
     [SerializeField] int playerScore = 0;
     [SerializeField] int enemyScore = 0;
+
+    enum announcements
+    {
+        defaultMessage,
+        playerLoss,
+        playerWon
+    }
+
+    [SerializeField] announcements currentAnnouncment;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,5 +28,18 @@ public class UIController : MonoBehaviour
     void Update()
     {
         ScoreText.text = "Player: " + playerScore + " Enemy: " + enemyScore;
+
+        switch(currentAnnouncment)
+        {
+            case announcements.defaultMessage:
+                announcerText.text = "";
+                break;
+            case announcements.playerLoss:
+                announcerText.text = "You lose";
+                break;
+            case announcements.playerWon:
+                announcerText.text = "You won!!";
+                break;
+        }
     }
 }
