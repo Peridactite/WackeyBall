0bcab9e3288f47a709d36a54e3e67fc0b43aa65d
Connor Kline <32586807+ckline1@users.noreply.github.com>
2019-11-05 11:07:44 -0500

    Added basic timer

--- Assets/UIController.cs
+++ Assets/UIController.cs
@@ -7,8 +7,14 @@ public class UIController : MonoBehaviour
 {
     [SerializeField] Text ScoreText;
     [SerializeField] Text announcerText;
+    [SerializeField] Text timerText;
+
     [SerializeField] int playerScore = 0;
     [SerializeField] int enemyScore = 0;
+    [SerializeField] float timer = 180;
+
+    private bool canCount = true;
+    private bool doOnce = false;
 
     enum announcements
     {
@@ -40,6 +46,20 @@ public class UIController : MonoBehaviour
             case announcements.playerWon:
                 announcerText.text = "You won!!";
                 break;
+        }
+
+        if (timer >= 0.0f && canCount)
+        {
+            timer -= Time.deltaTime;
+            timerText.text = timer.ToString("F");
+        }
+
+        else if(timer <= 0.0f && !doOnce)
+        {
+            canCount = false;
+            doOnce = true;
+            timerText.text = "0:00";
+            currentAnnouncment = announcements.playerWon;
         }
     }
 }
